4e77a3e2235e5595a5feac8ce1e8619a28d9c55b
Mack-0409 <mayanksingh0409@gmail.com>
2026-02-20 00:39:22 +0530

    Add a button to top

---
+++ Watch_Man_Series/frontend/src/components/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cartPageStyles from '../assets/dummyStyles';
+
+const CartPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartPage
