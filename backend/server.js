8dd388923159a005f4e12cd37a9a07b669e6394f
Mack-0409 <mayanksingh0409@gmail.com>
2026-03-02 23:49:14 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { connectDB } from './config/db.js';
 
 const app = express();
-const port = 4000;
+const  port = 4000;
 
-// MIDDLEWARES
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // DB
-connectDB();
 
 // ROUTES
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("API WORKING");
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on https://localhost:${port}`)
+    console.log(`Server tarted on https://localhost:${port}`)
 });
