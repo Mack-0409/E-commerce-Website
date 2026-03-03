412e9d6556d13fde5fe3e036c37447d35eaa9fb1
Mack-0409 <mayanksingh0409@gmail.com>
2026-03-04 00:46:33 +0530

    watchModel done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
+import path from 'path';
+import userRouter from './routes/userRouter.js';
+import watchRouter from './routes/watchRoute.js';
+
 
 const app = express();
 const  port = 4000;
@@ -15,6 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // ROUTES
+app.use("/api/auth", userRouter);
+app.use("/uploads", express.static(Path.join(process.cwd(), "uploads")));
+app.use("/api/watches", watchRouter);
 
 app.get('/', (req, res) => {
     res.send("API WORKING");
