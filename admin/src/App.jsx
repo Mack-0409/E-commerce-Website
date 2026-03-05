829b3c39ba467b62e7f6657d9239c2297d4d43cc
Mack-0409 <mayanksingh0409@gmail.com>
2026-03-05 23:24:57 +0530

    List Page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add'
+import List from './pages/List'
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Add />} />
+        <Route path='/list' element={<List />} />
       </Routes> 
     </>
   )
